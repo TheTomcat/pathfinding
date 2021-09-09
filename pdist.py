0f53b36f5111e162d29bd98b5e02a850ca661074
Thomas Vos <vossy01@gmail.com>
2021-09-10 07:17:17 +1000

    Added plotting and criticals

--- pdist.py
+++ pdist.py
@@ -1,5 +1,23 @@
 import random
 from collections import defaultdict
+import operator as op
+
+def droplow(ndrop=1):
+    def dropnlow(nums):
+        return tuple(sorted(nums)[ndrop:])
+    return dropnlow
+def drophigh(nums, ndrop=1):
+    def dropnhigh(nums):
+        return tuple(sorted(nums)[:-ndrop])
+    return dropnhigh
+def adv(nums):
+    return max(nums)
+def disadv(nums):
+    return min(nums)
+def reliable(nums, av=6):
+    return None
+def sumt(nums):
+    return (sum(nums),)
 
 class PDist(object):
     def __init__(self, outcomes):
@@ -13,6 +31,10 @@ class PDist(object):
             outcomes = {(outcome,): 1 for outcome in outcomes}
         self._outcomes = {outcome:weight for outcome, weight in outcomes.items()}
     
+    @classmethod
+    def dice_roll(cls, n):
+        return cls(list(range(1,n+1)))
+
     @property
     def total_weight(self) -> float:
         """The total weight"""
@@ -21,7 +43,7 @@ class PDist(object):
     def normalise(self) -> None:
         """Rescale all the weights so that they sum to 1"""
         tot = self.total_weight
-        self._outcomes = {outcome:weight/tot for outcome, weight in self._outcomes}
+        self._outcomes = {outcome:weight/tot for outcome, weight in self._outcomes.items()}
 
     def __add__(self, other):
         if isinstance(other, (int, float)):
@@ -50,7 +72,7 @@ class PDist(object):
         #         outcomes[a+b] = w
         # return PDist(outcomes)
     
-    def map_outcomes(pdist: "PDist", f=sum) -> "PDist":
+    def map_outcomes(pdist: "PDist", f=sumt) -> "PDist":
         """Take a PDist and map the 'outcome' according to some function f. Some possibilities involve:
         pdist.adv - takes the max of the outcomes
         pdist.disadv - takes the min of the outcomes
@@ -69,24 +91,105 @@ class PDist(object):
 
         return PDist(output)
 
+    def reduce_outcomes(self):
+        self = self.map_outcomes()
+
     def select(self, n=1, rng: random.Random=None) -> list:
         if rng is None:
             rng = random.Random()
         return rng.choices([i for i in self._outcomes.keys()], [i for i in self._outcomes.values()], k=n)
+    
+    def min(self):
+        return min(map(sum, self._outcomes))
+    def max(self):
+        return max(map(sum, self._outcomes))
 
-def droplow(nums, ndrop=1):
-    return sum(sorted(nums)[ndrop:])
-def drophigh(nums, ndrop=1):
-    return sum(sorted(nums)[:-ndrop])
-def adv(nums):
-    return max(nums)
-def disadv(nums):
-    return min(nums)
+    def average(self):
+        totw = sum([w for w in self._outcomes.values()])
+        return sum([sum(outc)*weight for outc, weight in self._outcomes.items()])/totw
 
-if __name__ == "__main__":
-    d6 = PDist([1,2,3,4,5,6])
-    d6_2 = d6+d6
-    adv_d6 = d6_2.map_outcomes(adv)
-    disadv_d6 = d6_2.map_outcomes(disadv)
+    def plot(self):
+        p = self.map_outcomes()
+        from matplotlib import pyplot as plt
+        plt.bar(list(map(sum, p._outcomes.keys())), p._outcomes.values())
+        plt.show()
 
-    skills = (d6+d6+d6+d6).map_outcomes(droplow)
+    def aplot(self, ysc=15, pad_h = 3):
+        pad=pad_h
+        def x_axis(n=6, pad=3):
+            ticks = ('-'*(pad-1)+'+')*n
+            labels = ''.join([f'{i+1:{pad}}' for i in range(n)])
+            return ticks, labels
+        dy = max(self._outcomes.values()) / ysc
+        cy = 0
+        cont=True
+        rows = []
+        while cont:
+            cont=False
+            cy += dy
+            row = ['#' if i>=cy else ' ' for i in self._outcomes.values()]
+            if "#" in row:
+                cont = True
+            rows.append(row)
+        print('^')
+        for row in reversed(rows):
+            print('|' + (' '*(pad-1)) + ''.join([f'{dit:{pad}}' for dit in row]))
+        axis, labels = x_axis(n=len(self._outcomes), pad=pad)
+        print('+' + axis + '->')
+        print(' ' + labels)
+
+def crit(pdist: PDist, crit_on=20, max_on=None):
+    roll_twice = (pdist+pdist).map_outcomes()
+    # print('roll_twice: ', roll_twice)
+    new_outcomes = list(range(pdist.min(), 2*pdist.max()+1))
+    # print('all outcomes:', new_outcomes)
+    new_pdist = PDist(new_outcomes)
+    pdist.normalise()
+    if max_on is None:
+        max_chance = 0
+        crit_chance = (21-crit_on)/20
+        norm_chance = (crit_on-1)/20
+    else:
+        max_chance = (21-max_on)/20
+        crit_chance = (max_on-crit_on)/20
+        norm_chance = (crit_on-1)/20
+    max_outcome = max(new_pdist._outcomes.keys())
+    for outcome in new_pdist._outcomes.keys():
+        n_prob = pdist._outcomes.get(outcome, 0)
+        c_prob = roll_twice._outcomes.get(outcome, 0)
+        if outcome == max_outcome:
+            m_prob=1
+        else:
+            m_prob=0
+        # print(outcome, ': ', n_prob, c_prob)
+        new_pdist._outcomes[outcome] = norm_chance*n_prob + crit_chance * c_prob + max_chance * m_prob
+    return new_pdist
+
+
+
+# if __name__ == "__main__":
+d4 = PDist.dice_roll(4) # ([1,2,3,4])
+d6 = PDist.dice_roll(6) # ([1,2,3,4,5,6])
+d8 = PDist.dice_roll(8) # ([1,2,3,4,5,6,7,8])
+d10 = PDist.dice_roll(10) # ([1,2,3,4,5,6,7,8,9,10])
+d12 = PDist.dice_roll(12) # ([1,2,3,4,5,6,7,8,9,10,11,12])
+
+d4_2 = (d4+d4).map_outcomes()
+d6_2 = (d6+d6).map_outcomes()
+d8_2 = (d8+d8).map_outcomes()
+d10_2 = (d10+d10).map_outcomes()
+d12_2 = (d12+d12).map_outcomes()
+
+d4_adv = (d4+d4).map_outcomes(adv)
+d4_disadv = (d4+d4).map_outcomes(disadv)
+d6_adv = (d6+d6).map_outcomes(adv)
+d6_disadv = (d6+d6).map_outcomes(disadv)
+d8_adv = (d8+d8).map_outcomes(adv)
+d8_disadv = (d8+d8).map_outcomes(disadv)
+d10_adv = (d10+d10).map_outcomes(adv)
+d10_disadv = (d10+d10).map_outcomes(disadv)
+d12_adv = (d12+d12).map_outcomes(adv)
+d12_disadv = (d12+d12).map_outcomes(disadv)
+
+
+    
